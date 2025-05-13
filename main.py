e6c4563df744bba898b34a563ff8c944dcc29358
ivandonets1991 <ivandonets1991@gmail.com>
2025-05-13 17:08:38 +0300

    #4

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-class Human:
+# class Human:
+#     height = 170
+#     # satiety = 50
+# class Student(Human):
+#     satiety=0
+# class Worker(Human):
+#     satiety=100
+# nick = Student()
+# ann = Worker()
+# print(nick.satiety)
+# print(ann.satiety)
+
+class Grandparent:
     height = 170
-    # satiety = 50
-class Student(Human):
-    satiety=0
-class Worker(Human):
-    satiety=100
-nick = Student()
-ann = Worker()
-print(nick.satiety)
-print(ann.satiety)
+    satiety = 100
+    age = 60
+class Parent(Grandparent):
+    age = 40
+class Child(Parent):
+    height = 50
+    def __init__(self):
+        print(self.height)
+        print(self.satiety)
+        print(self.age)
+nick = Child()
