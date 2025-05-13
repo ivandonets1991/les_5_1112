4cb2fd951565d86e7ed90994166484350f7f20ce
ivandonets1991 <ivandonets1991@gmail.com>
2025-05-13 17:13:13 +0300

    #5

--- main.py
+++ main.py
@@ -10,16 +10,44 @@
 # print(nick.satiety)
 # print(ann.satiety)
 
-class Grandparent:
-    height = 170
-    satiety = 100
-    age = 60
-class Parent(Grandparent):
-    age = 40
-class Child(Parent):
-    height = 50
-    def __init__(self):
-        print(self.height)
-        print(self.satiety)
-        print(self.age)
-nick = Child()
+# class Grandparent:
+#     height = 170
+#     satiety = 100
+#     age = 60
+# class Parent(Grandparent):
+#     age = 40
+# class Child(Parent):
+#     height = 50
+#     def __init__(self):
+#         print(self.height)
+#         print(self.satiety)
+#         print(self.age)
+# nick = Child()
+
+# class Hello_world:
+#     hello = "Hello"
+#     _hello = "_Hello"
+#     __hello = "__Hello"
+#     def __init__(self):
+#         self.world = "World"
+#         self._world = "_World"
+#         self.__world = "__World"
+#     def printer(self):
+#         print(self.hello)
+#         print(self._hello)
+#         print(self.__hello)
+#         print(self.world)
+#         print(self._world)
+#         print(self.__world)
+# class Hi(Hello_world):
+#     def hi_print(self):
+#         print(self.hello)
+#         print(self.world)
+#         print(self._hello)
+#         print(self._world)
+#         print(self.__hello)
+#         print(self.__world)
+# hello = Hello_world()
+# hello.printer()
+# hi = Hi()
+# hi.hi_print()
